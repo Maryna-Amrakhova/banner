c4b64dea3f4265f2312d76d82d4c39133259ad3c
Amrakhova Maryna <seagull2305@gmail.com>
2023-04-20 12:45:47 +0300

    new commits

---
+++ js/main.js
@@ -0,0 +1,16 @@
+		  gsap.from(".google" , {
+		  y:80,
+		  opacity:0,
+		  duration:2
+		});
+		  gsap.from(".itunes" , {
+		  x:80,
+		  opacity:0,
+		  duration:1
+		});
+		  gsap.from('h1', {
+		  duration: 2,
+		  x: 200,
+		  scale: 1
+		});
+	
